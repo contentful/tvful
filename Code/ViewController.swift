00f95797d0ce797e1677e3d5109912ac764a299b
Boris Bügling <boris@icculus.org>
2015-09-10 22:35:49 +0200

    Basic example which loads an asset from CTF

--- Code/ViewController.swift
+++ Code/ViewController.swift
@@ -6,20 +6,40 @@
 //  Copyright © 2015 Boris Bügling. All rights reserved.
 //
 
+import ContentfulDeliveryAPI
 import UIKit
 
 class ViewController: UIViewController {
+    static let AccessToken = "51910a9d4752a24728c44a8dc4422291305eed4c20000003b73d4a47475f1740"
+    static let SpaceId = "1qptv5yuwnfh"
+
+    let client = CDAClient(spaceKey: SpaceId, accessToken: AccessToken)
+    var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        imageView = UIImageView(frame: view.bounds)
+        view.addSubview(imageView)
+
+        client.fetchEntriesMatching(["content_type": "1xYw5JsIecuGE68mmGMg20"], success: { (_, entries) in
+            let imageUrls = entries.items.flatMap() { (entry) in
+                (entry.fields["photo"] as? CDAAsset)?.URL
+            }
+
+            let session = NSURLSession.sharedSession()
+            let task = session.downloadTaskWithURL(imageUrls[0], completionHandler: { (location, _, error) in
+                if let location = location, data = NSData(contentsOfURL: location) {
+                    self.imageView.image = UIImage(data: data)
+                }
+
+                if let error = error {
+                    print(error)
+                }
+            })
+            task.resume()
+        }) { (_, error) in
+            print(error)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
