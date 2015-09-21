5abe864455b275625dbe00a127728b77872f855e
Boris Bügling <boris@icculus.org>
2015-09-21 22:20:56 +0200

    Implemented minimal collection view of images

--- Code/ViewController.swift
+++ Code/ViewController.swift
@@ -9,37 +9,73 @@
 import ContentfulDeliveryAPI
 import UIKit
 
-class ViewController: UIViewController {
+class ImageCell: UICollectionViewCell {
+    static let identifier = NSStringFromClass(ImageCell.self)
+    let imageView = UIImageView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        imageView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        imageView.contentMode = .ScaleAspectFit
+        imageView.frame = bounds
+        contentView.addSubview(imageView)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("not implemented")
+    }
+
+    func loadAsset(asset: CDAAsset) {
+        let scale = UIScreen.mainScreen().scale
+        imageView.cda_setImageWithAsset(asset, size: CGSize(width: frame.size.width * scale, height: frame.size.height * scale))
+    }
+}
+
+class ViewController: UICollectionViewController {
     static let AccessToken = "51910a9d4752a24728c44a8dc4422291305eed4c20000003b73d4a47475f1740"
     static let SpaceId = "1qptv5yuwnfh"
 
     let client = CDAClient(spaceKey: SpaceId, accessToken: AccessToken)
-    var imageView: UIImageView!
+    var assets: [CDAAsset] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        imageView = UIImageView(frame: view.bounds)
-        view.addSubview(imageView)
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSize(width: 300, height: 300)
+        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        collectionView?.collectionViewLayout = flowLayout
+
+        collectionView?.registerClass(ImageCell.self, forCellWithReuseIdentifier: ImageCell.identifier)
 
         client.fetchEntriesMatching(["content_type": "1xYw5JsIecuGE68mmGMg20"], success: { (_, entries) in
-            let imageUrls = entries.items.flatMap() { (entry) in
-                (entry.fields["photo"] as? CDAAsset)?.URL
+            self.assets = entries.items.flatMap() { (entry) in
+                (entry.fields["photo"] as? CDAAsset)
             }
 
-            let session = NSURLSession.sharedSession()
-            let task = session.downloadTaskWithURL(imageUrls[0], completionHandler: { (location, _, error) in
-                if let location = location, data = NSData(contentsOfURL: location) {
-                    self.imageView.image = UIImage(data: data)
-                }
-
-                if let error = error {
-                    print(error)
-                }
-            })
-            task.resume()
+            self.collectionView?.reloadData()
         }) { (_, error) in
             print(error)
         }
+    }
+
+    override func collectionView(collectionView: UICollectionView, canFocusItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(ImageCell.identifier, forIndexPath: indexPath) as! ImageCell
+        cell.loadAsset(assets[indexPath.row])
+        //cell.performSelector("_whyIsThisViewNotFocusable")
+        return cell
+    }
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return assets.count
+    }
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
     }
 }
